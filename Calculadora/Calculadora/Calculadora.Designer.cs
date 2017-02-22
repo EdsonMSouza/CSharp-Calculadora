8a7646319a89f53413c28ef2de1ee62762ac66eb
Edson Melo de Souza <souzaem@outlook.com>
2017-02-22 08:01:33 -0300

    Manipulação de variáveis

---
+++ Calculadora/Calculadora/Calculadora.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Calculadora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.lblValor2 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.btAdicao = new System.Windows.Forms.Button();
+            this.btSubtracao = new System.Windows.Forms.Button();
+            this.btMultiplicacao = new System.Windows.Forms.Button();
+            this.btDivisao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Location = new System.Drawing.Point(12, 19);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(40, 13);
+            this.lblValor1.TabIndex = 0;
+            this.lblValor1.Text = "Valor 1";
+            // 
+            // lblValor2
+            // 
+            this.lblValor2.AutoSize = true;
+            this.lblValor2.Location = new System.Drawing.Point(227, 19);
+            this.lblValor2.Name = "lblValor2";
+            this.lblValor2.Size = new System.Drawing.Size(40, 13);
+            this.lblValor2.TabIndex = 1;
+            this.lblValor2.Text = "Valor 2";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(58, 15);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(100, 20);
+            this.txtValor1.TabIndex = 2;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(273, 15);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(100, 20);
+            this.txtValor2.TabIndex = 3;
+            // 
+            // btAdicao
+            // 
+            this.btAdicao.Location = new System.Drawing.Point(15, 61);
+            this.btAdicao.Name = "btAdicao";
+            this.btAdicao.Size = new System.Drawing.Size(85, 23);
+            this.btAdicao.TabIndex = 4;
+            this.btAdicao.Text = "Adição";
+            this.btAdicao.UseVisualStyleBackColor = true;
+            this.btAdicao.Click += new System.EventHandler(this.btAdicao_Click);
+            // 
+            // btSubtracao
+            // 
+            this.btSubtracao.Location = new System.Drawing.Point(106, 61);
+            this.btSubtracao.Name = "btSubtracao";
+            this.btSubtracao.Size = new System.Drawing.Size(85, 23);
+            this.btSubtracao.TabIndex = 5;
+            this.btSubtracao.Text = "Subtração";
+            this.btSubtracao.UseVisualStyleBackColor = true;
+            this.btSubtracao.Click += new System.EventHandler(this.btSubtracao_Click);
+            // 
+            // btMultiplicacao
+            // 
+            this.btMultiplicacao.Location = new System.Drawing.Point(197, 61);
+            this.btMultiplicacao.Name = "btMultiplicacao";
+            this.btMultiplicacao.Size = new System.Drawing.Size(85, 23);
+            this.btMultiplicacao.TabIndex = 6;
+            this.btMultiplicacao.Text = "Multiplicação";
+            this.btMultiplicacao.UseVisualStyleBackColor = true;
+            this.btMultiplicacao.Click += new System.EventHandler(this.btMultiplicacao_Click);
+            // 
+            // btDivisao
+            // 
+            this.btDivisao.Location = new System.Drawing.Point(288, 61);
+            this.btDivisao.Name = "btDivisao";
+            this.btDivisao.Size = new System.Drawing.Size(85, 23);
+            this.btDivisao.TabIndex = 7;
+            this.btDivisao.Text = "Divisão";
+            this.btDivisao.UseVisualStyleBackColor = true;
+            this.btDivisao.Click += new System.EventHandler(this.btDivisao_Click);
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 98);
+            this.Controls.Add(this.btDivisao);
+            this.Controls.Add(this.btMultiplicacao);
+            this.Controls.Add(this.btSubtracao);
+            this.Controls.Add(this.btAdicao);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.lblValor2);
+            this.Controls.Add(this.lblValor1);
+            this.Name = "frmCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora - Manipulação de Variáveis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.Label lblValor2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Button btAdicao;
+        private System.Windows.Forms.Button btSubtracao;
+        private System.Windows.Forms.Button btMultiplicacao;
+        private System.Windows.Forms.Button btDivisao;
+    }
+}
+
---
+++ Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btAdicao_Click(object sender, EventArgs e)
+        {
+            /* Declarando as variáveis para manipular os valores e o resultado */
+            int valor1 = 0;
+            int valor2 = 0;
+            int resultado = 0;
+
+            /* Recuperando o valor digitado nas caixas de texto, convertendo para inteiro e atribuindo às variáveis valor1 e valor2.
+             * Neste procedimento existe um Tratamento de Erros realizado pela estrutura:
+             * 
+             * try{
+             *      Tenta realizar a operação e, caso não consiga envia para o bloco a seguir
+             * }
+             * catch (FormatException fe)
+             * {
+             *      Aqui você pode exibir uma mensagem personalizada, ou então usar a variável "fe" para apresentar o erro
+             * }
+            */
+            try
+            {
+                valor1 = Convert.ToInt32(txtValor1.Text);
+                valor2 = Convert.ToInt32(txtValor2.Text);
+
+                /* Realizando a operação*/
+                resultado = (valor1 + valor2);
+
+                /* Exibindo resultado em uma caixa de Mensagem */
+                /* O primeiro parâmetro é o valor ser mostrado no Corpo da caixa e o segundo o Título*/
+                MessageBox.Show(resultado.ToString(), "Resultado da Operação");
+
+                /* Uma opção mais elabora pode ser vista no código a seguir, após clicar em OK na caixa anterior */
+                String textoResultado = "A soma entre (" + txtValor1.Text + " e " + txtValor2.Text + ") é = ";
+                MessageBox.Show(textoResultado + resultado.ToString(), "Resultado da Operação");
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Os valores devem ser números Inteiros!\n" + fe.Message, "ATENÇÃO");
+            }
+        }
+
+        private void btSubtracao_Click(object sender, EventArgs e)
+        {
+            /* Declarando as variáveis para manipular os valores e o resultado */
+            int valor1 = 0;
+            int valor2 = 0;
+            int resultado = 0;
+
+            /* Recuperando o valor digitado nas caixas de texto, convertendo para inteiro e atribuindo às variáveis valor1 e valor2.
+             * Neste procedimento existe um Tratamento de Erros realizado pela estrutura:
+             * 
+             * try{
+             *      Tenta realizar a operação e, caso não consiga envia para o bloco a seguir
+             * }
+             * catch (FormatException fe)
+             * {
+             *      Aqui você pode exibir uma mensagem personalizada, ou então usar a variável "fe" para apresentar o erro
+             * }
+            */
+            try
+            {
+                valor1 = Convert.ToInt32(txtValor1.Text);
+                valor2 = Convert.ToInt32(txtValor2.Text);
+
+                /* Realizando a operação*/
+                resultado = (valor1 - valor2);
+
+                /* Exibindo resultado em uma caixa de Mensagem */
+                /* O primeiro parâmetro é o valor ser mostrado no Corpo da caixa e o segundo o Título*/
+                MessageBox.Show(resultado.ToString(), "Resultado da Operação");
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Os valores devem ser números Inteiros!\n" + fe.Message, "ATENÇÃO");
+            }
+        }
+
+        private void btMultiplicacao_Click(object sender, EventArgs e)
+        {
+            /* Declarando as variáveis para manipular os valores e o resultado */
+            int valor1 = 0;
+            int valor2 = 0;
+            int resultado = 0;
+
+            /* Recuperando o valor digitado nas caixas de texto, convertendo para inteiro e atribuindo às variáveis valor1 e valor2.
+             * Neste procedimento existe um Tratamento de Erros realizado pela estrutura:
+             * 
+             * try{
+             *      Tenta realizar a operação e, caso não consiga envia para o bloco a seguir
+             * }
+             * catch (FormatException fe)
+             * {
+             *      Aqui você pode exibir uma mensagem personalizada, ou então usar a variável "fe" para apresentar o erro
+             * }
+            */
+            try
+            {
+                valor1 = Convert.ToInt32(txtValor1.Text);
+                valor2 = Convert.ToInt32(txtValor2.Text);
+
+                /* Realizando a operação*/
+                resultado = (valor1 * valor2);
+
+                /* Exibindo resultado em uma caixa de Mensagem */
+                /* O primeiro parâmetro é o valor ser mostrado no Corpo da caixa e o segundo o Título*/
+                MessageBox.Show(resultado.ToString(), "Resultado da Operação");
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Os valores devem ser números Inteiros!\n" + fe.Message, "ATENÇÃO");
+            }
+        }
+
+        private void btDivisao_Click(object sender, EventArgs e)
+        {
+            /* Declarando as variáveis para manipular os valores e o resultado */
+            double valor1 = 0;
+            double valor2 = 0;
+            double resultado = 0;
+
+            /* Recuperando o valor digitado nas caixas de texto, convertendo para inteiro e atribuindo às variáveis valor1 e valor2.
+             * Neste procedimento existe um Tratamento de Erros realizado pela estrutura:
+             * 
+             * try{
+             *      Tenta realizar a operação e, caso não consiga envia para o bloco a seguir
+             * }
+             * catch (FormatException fe)
+             * {
+             *      Aqui você pode exibir uma mensagem personalizada, ou então usar a variável "fe" para apresentar o erro
+             * }
+            */
+            try
+            {
+                valor1 = Convert.ToDouble(txtValor1.Text);
+                valor2 = Convert.ToDouble(txtValor2.Text);
+
+                /* Realizando a operação*/
+                resultado = (valor1 / valor2);
+
+                /* Exibindo resultado em uma caixa de Mensagem */
+                /* O primeiro parâmetro é o valor ser mostrado no Corpo da caixa e o segundo o Título*/
+                MessageBox.Show(resultado.ToString(), "Resultado da Operação");
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show(fe.Message, "ATENÇÃO");
+            }
+        }
+    }
+}
